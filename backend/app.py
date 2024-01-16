0595003ec5f89cd21b864ab2ad1a4d2bcfb5a7f4
jwagn037 <jaredaustin.wagner@gmail.com>
2024-01-16 04:39:25 -0500

    Add pseudocache functionality
    The pseudocache is a folder containing HTML .txt files corresponding to
    previous URLs we have made requests to. Each file's name is a SHA-256
    hash of its URL to ensure that (1) they are composed on allowed
    characters (2) each file is given a unique name.
    
    The primary purpose of the pseudocache is reducing how many requests we
    send to test URLs. Notably, it does not elimiate the necessity of GET
    requests since many assets are purposefully designed to load late by
    many web developers.
    
    The secondary purpose is to use this as a "proof of concept," because
    loading these pages from memory is noticeably faster than querying for
    them every time we submit a URL.

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template
 import requests
 from bs4 import BeautifulSoup
 import validators
+import hashlib
+import os
 
 app = Flask(__name__)
 
@@ -14,9 +16,13 @@ def submit_data():
     # parse URL from submission, get response
     try:
         if request.method == 'POST':
+            url = request.form['data']
             if validators.url(url): # check that the requested url is valid
-                url = request.form['data']
-                response = requests.get(url)
+                find_in_cache = in_cache(url)
+                if find_in_cache == False:
+                    http = save_to_cache(url)
+                else:
+                    http = find_in_cache
             else:
                 return 'URL is invalid'
         else:
@@ -26,12 +32,46 @@ def submit_data():
     
     # parse response, get html
     try:
-        if response.status_code == 200:
-            http = response.text
-            soup = BeautifulSoup(http, 'html.parser')
+        soup = BeautifulSoup(http, 'html.parser')
         return soup.prettify()
     except Exception as e:
         return f'An error occurred: {str(e)}'
 
 if __name__ == "__main__":
     app.run(debug=True) # this should be False in a prod environment
+    
+    # Saves an HTTP response to the pseudocache.
+def save_to_cache(url):
+    directory = 'pseudocache'
+    # use SHA-256 hash to create a unique filename
+    filename = os.path.join(directory, hashlib.sha256(url.encode()).hexdigest())
+    
+    response = requests.get(url)
+    http = response.text
+    
+    os.makedirs(directory, exist_ok=True)  # Create the directory if it doesn't exist
+    
+    if response.status_code == 200:
+        with open(filename, mode='w') as localfile:
+            print("Saving "+ filename)
+            localfile.write(http)
+    
+    return http
+
+    # Reads the pseudocache. Returns False if a provided URL does not
+    # correpond to a file there, OR a soup if we saved it already.
+    # Just exists to reduce the number of queries we make to websites.
+    # Anything else is impolite.
+def in_cache(url):
+    directory = 'pseudocache'
+    # use SHA-256 hash to create a unique filename
+    filename = os.path.join(directory, hashlib.sha256(url.encode()).hexdigest())
+    
+    try:
+        with open(filename, 'r') as file:
+            print("Reading " + filename)
+            http = file.read()
+            return http
+    except Exception as e:
+        print(e)
+        return False
