68f49abe426badf901fc8eaf5b412807f359a008
jwagn037 <jaredaustin.wagner@gmail.com>
2024-02-05 18:23:18 -0500

    Refactor cache logic
    Small refactoring of the cache logic to follow DRY principle re:SHA256
    hashing. I might still ombine these functions since they are more
    verbose than necessary (they still repeat themselves in how they
    read/write to the cache.)

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+################################### IMPORTS ###################################
 from flask import Flask, request
 import requests
 from bs4 import BeautifulSoup
@@ -5,6 +6,8 @@ from trafilatura import extract
 import validators
 import hashlib
 import os
+
+################################### FLASK ###################################
 
 app = Flask(__name__)
 
@@ -20,16 +23,17 @@ def scrape_url():
         return "Invalid request"
     
     url = str(request.args.get('data'))
-    print("GET request for: ", url)
+    print("GET: ", url)
     
     # check that the requested URL is valid
     # validators: https://pypi.org/project/validators/
     if (validators.url(url) == False):
         return "Invalid URL"
     
-    # check if we have this URL HTML cached
+    # check if we have this URL HTML cached and set http
+    # http is a string!
     if in_cache(url):
-        http = in_cache(url)
+        http = read_cache(url)
     else:
         http = save_to_cache(url)
     
@@ -38,37 +42,50 @@ def scrape_url():
         return parse_response(http, mode=0)
     except Exception as e:
         return f'An error occurred: {str(e)}'
+
+# CORS-ish: https://stackoverflow.com/questions/19962699/flask-restful-cross-domain-issue-with-angular-put-options-methods
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET')
+    return response
+
+################################### FUNCTIONS ###################################
     
-    # Saves an HTTP response to the pseudocache.
+    # Returns: <dir>\<SHA-256 URL hash>
+def locate_url(url, directory='pseudocache'):
+    os.makedirs(directory, exist_ok=True)  # Create the directory if it doesn't exist
+    return os.path.join(directory, hashlib.sha256(url.encode()).hexdigest())
+
+    # Saves HTML doc to cache.
 def save_to_cache(url):
-    directory = 'pseudocache'
-    # use SHA-256 hash to create a unique filename
-    filename = os.path.join(directory, hashlib.sha256(url.encode()).hexdigest())
+    file_path = locate_url(url)
     
     response = requests.get(url)
-    http = response.text
-    
-    os.makedirs(directory, exist_ok=True)  # Create the directory if it doesn't exist
+    html = response.text
     
     if response.status_code == 200:
-        with open(filename, mode='w', encoding='utf-8') as localfile:
-            print("Saving "+ filename)
-            localfile.write(http)
+        with open(file_path, mode='w', encoding='utf-8') as localfile:
+            print("Save:", file_path)
+            localfile.write(html)
+            print("Saved:", file_path)
+    else:
+        print("Save FAIL: Invalid status_code:", response.status_code)
     
-    return http
+    return html
 
-    # Reads the pseudocache. Returns False if a provided URL does not
-    # correpond to a file there, OR a soup if we saved it already.
-    # Just exists to reduce the number of queries we make to websites.
-    # Anything else is impolite.
+    # Returns: True (file exsist) OR False (file does not exist)
 def in_cache(url):
-    directory = 'pseudocache'
-    # use SHA-256 hash to create a unique filename
-    filename = os.path.join(directory, hashlib.sha256(url.encode()).hexdigest())
+    file_path = locate_url(url)
     
+    return os.path.isfile(file_path)
+    
+def read_cache(url):
+    file_path = locate_url(url)
     try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            print("Reading " + filename)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            print("Read:", file_path)
             http = file.read()
             return http
     except Exception as e:
@@ -78,7 +95,7 @@ def in_cache(url):
     # mode == 0: BS4 parsing
     # mode == 1: Trafilatura parsing
 def parse_response(http, mode):
-    json_article = "\{\}"
+    json_article = "{}" ## default empty JSON
     
     if (mode == 0):
         soup = BeautifulSoup(http, 'html.parser')
@@ -114,11 +131,3 @@ def write_json(text, title='',author='',date=''):
         
     json_article['content'] = content
     return json_article
-
-# CORS-ish: https://stackoverflow.com/questions/19962699/flask-restful-cross-domain-issue-with-angular-put-options-methods
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET')
-    return response
